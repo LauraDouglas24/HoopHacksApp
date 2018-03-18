edf0c9c39b6d758c4cadcd15b08fff4c090ab417
Laura Douglas <lauradouglas@Lauras-MacBook-Pro-2.local>
2018-03-18 17:53:48 +0000

    Removing unused imports

--- app/src/main/java/com/hoophacks/hoophacks3/Register.java
+++ app/src/main/java/com/hoophacks/hoophacks3/Register.java
@@ -1,18 +1,14 @@
 package com.hoophacks.hoophacks3;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -123,7 +119,6 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
                     }
                 });
     }
-
 
 }
 
