c23cfceebf723e5ea581869bc7dd1f250afa974e
Laura Douglas <laura24douglas@gmail.com>
2018-04-03 20:00:03 +0100

    Updating Exercise List Layout, adding additional field & adding menu to activity

--- app/src/main/java/com/hoophacks/hoophacks3/ExerciseList.java
+++ app/src/main/java/com/hoophacks/hoophacks3/ExerciseList.java
@@ -1,17 +1,22 @@
 package com.hoophacks.hoophacks3;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,7 +36,10 @@ public class ExerciseList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise_list);
+        setContentView(R.layout.activity_exercise);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         rvExercises = findViewById(R.id.rvExercises);
         rvExercises.setHasFixedSize(true);
@@ -50,16 +58,16 @@ public class ExerciseList extends AppCompatActivity {
                     protected void populateViewHolder(final ExerciseViewHolder viewHolder, Exercise model, int position) {
 
                         final String exerciseName = getRef(position).getKey();
-                        Log.i(TAG, exerciseName.toString());
 
                         // Read from the database
                         myRef.child(exerciseName).addValueEventListener(new ValueEventListener() {
-
                             @Override
                             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                                Log.i(TAG, exerciseName.toString());
                                 viewHolder.setName(dataSnapshot.getKey());
                                 viewHolder.setImage(Uri.parse(dataSnapshot.getValue(Exercise.class).getImage()));
+                                viewHolder.setSkillLevel(dataSnapshot.getValue(Exercise.class).getSkillLevel());
+
                             }
 
                             @Override
@@ -73,6 +81,13 @@ public class ExerciseList extends AppCompatActivity {
 
         rvExercises.setAdapter(firebaseRecyclerAdapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the MainMenu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_exercise_list, menu);
+        return true;
     }
 
     public static class ExerciseViewHolder extends RecyclerView.ViewHolder{
@@ -89,10 +104,39 @@ public class ExerciseList extends AppCompatActivity {
             tvExercise.setText(name);
         }
 
+        public void setSkillLevel(String skillLevel){
+            TextView tvSkillLevel = (TextView) mView.findViewById(R.id.tvSkillLevel);
+            tvSkillLevel.setText(skillLevel);
+        }
+
         public void setImage(Uri uri){
             ImageView ivExercise = mView.findViewById(R.id.ivExercise);
             Glide.with(mView.getContext()).load(uri).into(ivExercise);
         }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent myIntent = new Intent(ExerciseList.this, Login.class);
+                ExerciseList.this.startActivity(myIntent);
+                break;
+            case R.id.action_skill_areas:
+                Intent skillIntent = new Intent(ExerciseList.this, SkillAreas.class);
+                ExerciseList.this.startActivity(skillIntent);
+                break;
+            case R.id.action_user_profile:
+                Intent profileIntent = new Intent(ExerciseList.this, UserProfile.class);
+                ExerciseList.this.startActivity(profileIntent);
+            case R.id.action_settings:
+                Intent settingIntent = new Intent(ExerciseList.this, Settings.class);
+                ExerciseList.this.startActivity(settingIntent);
+                break;
+        }
+        return false;
     }
 }
 
--- app/src/main/java/com/hoophacks/hoophacks3/model/Exercise.java
+++ app/src/main/java/com/hoophacks/hoophacks3/model/Exercise.java
@@ -5,15 +5,17 @@ import android.net.Uri;
 public class Exercise {
     String image;
     String skillArea;
+    String skillLevel;
     String tip;
 
     public Exercise(){
 
     }
 
-    public Exercise(String image, String skillArea, String tip){
+    public Exercise(String image, String skillArea, String skillLevel, String tip){
         this.image = image;
         this.skillArea = skillArea;
+        this.skillLevel = skillLevel;
         this.tip = tip;
     }
 
@@ -31,6 +33,14 @@ public class Exercise {
 
     public void setSkillArea(String skillArea) {
         this.skillArea = skillArea;
+    }
+
+    public String getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(String skillLevel) {
+        this.skillLevel = skillLevel;
     }
 
     public String getTip() {
